f7c6ccdae54e12818e900ba977993b8fa470d33b
mahir29 <mahirhardwar@gmail.com>
2020-09-24 15:12:19 +0530

    Updates

---
+++ precache-manifest.2c93de3faeabf4c589ce8c8f2e1b338b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70244174929d002b5316bd47118001a",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "e65087af57b0ae174a0f",
+    "url": "/NewsApp/static/css/main.08f172fe.chunk.css"
+  },
+  {
+    "revision": "86359cbd51858ce6dc5d",
+    "url": "/NewsApp/static/js/2.a65afb9a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/NewsApp/static/js/2.a65afb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65087af57b0ae174a0f",
+    "url": "/NewsApp/static/js/main.9b1ca6aa.chunk.js"
+  },
+  {
+    "revision": "22ee77e043d83bf33cac",
+    "url": "/NewsApp/static/js/runtime-main.a74de4f6.js"
+  }
+]);
